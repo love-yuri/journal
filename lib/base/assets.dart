a09637d63708bb8e612199c20ff465da87fab8f2
love-yuri <yuri2078170658@gmail.com>
2024-06-23 23:08:19 +0800

    feat: 添加背景图片和AssetsManager类

---
+++ lib/base/assets.dart
@@ -0,0 +1,9 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-06-23 21:50:42
+ * @LastEditTime: 2024-06-23 22:44:41
+ * @Description: 静态文件加载
+ */
+class AssetsManager {
+  static const String homeBg = "assets/bg/ec006809-cb34-42b9-bc80-5902d06419f7.jpg";
+}
