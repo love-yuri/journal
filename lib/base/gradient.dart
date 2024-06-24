8107942c6f8e0edeac0ef427f8f984b3520573d2
love-yuri <yuri2078170658@gmail.com>
2024-06-24 23:17:47 +0800

    feat(gradient): 更新渐变背景颜色

--- lib/base/gradient.dart
+++ lib/base/gradient.dart
@@ -1,28 +1,33 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-06-23 21:37:39
+ * @LastEditTime: 2024-06-24 19:28:54
+ * @Description: 
+ */
 import 'package:flutter/material.dart';
+import 'package:journal/utils/utils.dart';
 
 class GradientManager {
-  static final bg = LinearGradient(
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-    colors: [
-      Colors.white,
-      Colors.pink.shade50,
-      Colors.pink.shade100,
-      Colors.pink.shade200,
-      Colors.pink.shade100,
-      Colors.pink.shade50,
-      Colors.white,
-    ],
-    stops: const [0.0, 0.15, 0.3, 0.5, 0.7, 0.85, 1.0],
-  );
-
-  static const homeBg = LinearGradient(
+  static LinearGradient bg = LinearGradient(
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
     colors: [
-      Color(0xFFEE9CA7), // rgb(238, 156, 167)
-      Color(0xFFFFDDE1), // rgb(255, 221, 225)
+      Utils.hexToColor("DBEAFE"),
+      Utils.hexToColor("E3EAFE"),
+      Utils.hexToColor("9695CF"),
+      Utils.hexToColor("9695CF"),
+      Utils.hexToColor("9695CF"),
     ],
-    stops: [0.0, 1.0],
+    stops: const [0.0, 0.1, 0.45, 0.6, 0.7 ],
+  );
+
+  static LinearGradient homeBg = LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    colors: [
+      const  Color(0xFFFFDDE1),
+      Utils.hexToColor("#9695CF"),
+    ],
+    stops: const [0.0, 1.0],
   );
 }
