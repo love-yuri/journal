34fc4a80c9e7f8a12168f0aadf17e9d003099e4c
love-yuri <yuri2078170658@gmail.com>
2024-06-25 22:58:54 +0800

    feat(journal_input): 添加文本输入框与回调函数

--- lib/components/journal_input.dart
+++ lib/components/journal_input.dart
@@ -1,37 +1,25 @@
 import 'package:flutter/material.dart';
 
 class JournalInput extends StatefulWidget {
-  const JournalInput({super.key});
+  JournalInput({super.key, this.onChanged});
+  final _controller = TextEditingController();
+  final void Function(String)? onChanged;
 
   @override
   State<StatefulWidget> createState() {
     return _JournalInputState();
   }
+
+  String? get text => _controller.text;
+  set text (text) => _controller.text = text;
 }
 
 class _JournalInputState extends State<JournalInput> {
-  final _controller = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    // _controller.addListener(_onTextChanged);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.removeListener(_onTextChanged);
-  }
-
-  _onTextChanged() {
-    debugPrint("Text field value: ${_controller.text}");
-  }
-
   @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: _controller,
+      onChanged: widget.onChanged,
+      controller: widget._controller,
       maxLines: null,
       decoration: const InputDecoration(
         border: null,
@@ -46,5 +34,15 @@ class _JournalInputState extends State<JournalInput> {
         color: Colors.black,
       ),
     );
+  }
+}
+
+class JournalInputModel extends ChangeNotifier {
+  String _text = '';
+  String get text => _text;
+
+  void onChanged(String value) {
+    _text = value;
+    notifyListeners();
   }
 }
