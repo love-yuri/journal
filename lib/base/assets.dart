f0745900afb6a1ec9a292d02019a44d8d3c4c609
love-yuri <yuri2078170658@gmail.com>
2024-06-26 23:43:59 +0800

    feat: 添加 AssetsConfig 和 DataBaseConfig 类

--- lib/base/assets.dart
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: love-yuri yuri2078170658@gmail.com
- * @Date: 2024-06-23 21:50:42
- * @LastEditTime: 2024-06-23 22:44:41
- * @Description: 静态文件加载
- */
-class AssetsManager {
-  static const String homeBg = "assets/bg/1.jpg";
-}
---
+++ lib/base/config.dart
@@ -0,0 +1,34 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2024-06-23 21:50:42
+ * @LastEditTime: 2024-06-26 21:07:25
+ * @Description: 静态文件加载
+ */
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+// 静态文件配置
+class AssetsConfig {
+  static get homeBg => "assets/bg/1.jpg";
+}
+
+// 数据库设置
+class DataBaseConfig {
+  static Future<String> jorunalTableDbPath() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    return p.join(dbFolder.path, 'journal.db');
+  }
+
+  static Future<File> jorunalTableDbFile() async {
+    return File(await jorunalTableDbPath());
+  }
+}
+
+class WebdavConfig {
+  static get host => 'https://dav.jianguoyun.com';
+  static get username => '2078170658@qq.com';
+  static get password => 'anp5yqxr435upzhu';
+  static get folder => 'journal/';
+}
