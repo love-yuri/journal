ed43914c293089d9c3e0518ec3c404df6ba76b89
love-yuri <yuri2078170658@gmail.com>
2024-06-26 23:44:17 +0800

    feat(lib/moor/entiy/journal.dart): 修改日记实体类，引入新配置文件

--- lib/moor/entiy/journal.dart
+++ lib/moor/entiy/journal.dart
@@ -1,15 +1,15 @@
 /*
  * @Author: love-yuri yuri2078170658@gmail.com
  * @Date: 2024-06-24 22:19:03
- * @LastEditTime: 2024-06-24 22:41:35
+ * @LastEditTime: 2024-06-26 23:03:47
  * @Description: 日记实体类
  */
 // These additional imports are necessary to open the sqlite3 database
 import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:journal/base/config.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
 // ignore: depend_on_referenced_packages
 import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
@@ -46,10 +46,7 @@ class JournalDatabase extends _$JournalDatabase {
 LazyDatabase _openConnection() {
   // LazyDatabase util 让我们找到文件 async 的正确位置。
   return LazyDatabase(() async {
-    // 将数据库文件（此处名为 db.sqlite）放入文档文件夹中
-    // 对于您的应用程序。
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'journal.db'));
+    final file = await DataBaseConfig.jorunalTableDbFile();
 
     // 还可以解决旧 Android 版本的限制
     if (Platform.isAndroid) {
